bd5b8e92ac01fcd7d94bfd17414f256ebeac2a86
KristianPu <kristianpurgar@gmail.com>
2025-03-07 22:38:32 +0100

    Setup url shortener module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { validate } from './config/env';
+
+import { UrlShortenerModule } from './url-shortener/url-shortener.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      validate,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'mandm',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    UrlShortenerModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/url-shortener/url-shortener.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { UrlShortenerController } from './url-shortener.controller';
+import { UrlShortenerService } from './url-shortener.service';
+
+@Module({
+  controllers: [UrlShortenerController],
+  providers: [UrlShortenerService],
+})
+export class UrlShortenerModule {}
