775c1afcdf7454153e1d2e22a4d0c2defedb2e1a
KristianPu <kristianpurgar@gmail.com>
2025-03-08 11:18:23 +0100

    Amend

--- src/url-shortener/url-shortener.controller.ts
+++ src/url-shortener/url-shortener.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+
+import { UrlShortenerService } from './url-shortener.service';
 
 @Controller('url-shortener')
-export class UrlShortenerController {}
+export class UrlShortenerController {
+  constructor(private readonly urlShortenerService: UrlShortenerService) {}
+
+  @Post()
+  create() {
+    return;
+  }
+}
--- src/url-shortener/url-shortener.service.ts
+++ src/url-shortener/url-shortener.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UrlShortenerService {}
+export class UrlShortenerService {
+  constructor() {}
+
+  async create() {}
+}
