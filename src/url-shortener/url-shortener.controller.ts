2922fdecbe6483b8bb3a520334ef52fe5ffbaeda
KristianPu <kristianpurgar@gmail.com>
2025-03-10 13:00:21 +0100

    feat: Add Post and Get endpoints for url shrotening

--- src/url-shortener/url-shortener.controller.ts
+++ src/url-shortener/url-shortener.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 import { UrlShortenerService } from './url-shortener.service';
+import { ShortenUrlDto } from './dto/shorten-url.dto';
 
 @Controller('url-shortener')
 export class UrlShortenerController {
   constructor(private readonly urlShortenerService: UrlShortenerService) {}
 
   @Post()
-  create() {
-    return;
+  shorten(@Body() shortenUrlDto: ShortenUrlDto) {
+    return this.urlShortenerService.createShortUrl(shortenUrlDto);
+  }
+
+  @Get(':id')
+  getUrl(@Param('id') id: string) {
+    return this.urlShortenerService.getUrl(id);
   }
 }
--- src/url-shortener/url-shortener.service.ts
+++ src/url-shortener/url-shortener.service.ts
@@ -4,29 +4,37 @@ import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 
 import { Url } from '../schemas/url.schema';
+import { ShortenUrlDto } from './dto/shorten-url.dto';
 
 @Injectable()
 export class UrlShortenerService {
   constructor(@InjectModel(Url.name) private urlModel: Model<Url>) {}
 
-  async createShortUrl(originalUrl: string): Promise<Url> {
+  async createShortUrl(shortenUrlDto: ShortenUrlDto) {
+    const { originalUrl } = shortenUrlDto;
     const base = 'basetext';
     const urlId = nanoid();
 
+    const validateUrl = !this.validateUrl(originalUrl);
+    console.log(validateUrl);
+
     try {
-      let url = this.urlModel.findOne({ originalUrl });
-      console.log(url, 'NISTA', typeof url);
+      let url = await this.urlModel.findOne({ originalUrl }).exec();
       if (url) return url;
 
       const shortUrl = `${base}/${urlId}`;
-      const newUrl = new this.urlModel({ originalUrl, shortUrl, clicks: 0 });
-      return newUrl.save();
+      return new this.urlModel({
+        urlId,
+        originalUrl,
+        shortUrl,
+        clicks: 0,
+      }).save();
     } catch (error) {
       console.log(error);
     }
   }
 
-  async getUrl(urlId: number): Promise<string> {
+  async getUrl(urlId: string): Promise<string> {
     try {
       const url = await this.urlModel.findOne({ urlId });
       if (url) {
@@ -38,9 +46,9 @@ export class UrlShortenerService {
     }
   }
 
-  validateUrl(value: string) {
+  validateUrl(url: string) {
     return new RegExp(
       '/^(?:(?:(?:https?|ftp):)?\\/\\/)(?:\\S+(?::\\S*)?@)?(?:(?!(?:10|127)(?:\\.\\d{1,3}){3})(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))(?::\\d{2,5})?(?:[/?#]\\S*)?$/i',
-    ).test(value);
+    ).test(url);
   }
 }
